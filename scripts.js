870a4e712bfd2b157ef938d0f672d4ff0e6e5987
Tyler <twahl36@gmail.com>
2018-11-17 00:21:43 -0500

    Updated wfl images

--- scripts.js
+++ scripts.js
@@ -2,10 +2,14 @@ function updateModal(colNum) {
   if (colNum == 1) {
     $('.modal-title').text('What\'s For Lunch?');
     $('.modal-footer span').text('Beep');
+    $('#car-img1').attr('src', 'images/wfl1.png');
+    $('#car-img2').attr('src', 'images/wfl3.png');
   }
   else if (colNum == 2) {
     $('.modal-title').text('Unredd Posts');
     $('.modal-footer span').text('Beep');
+    $('#car-img1').attr('src', 'images/urp1.png');
+    $('#car-img2').attr('src', 'images/urp2.png');
   }
   else if (colNum == 3) {
     $('.modal-title').text('"Let Me Google That For You" Discord Bot');
