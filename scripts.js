92a25956f6805ebf81357913a0f8aab750e9fbc3
Tyler <twahl36@gmail.com>
2018-11-16 21:39:13 -0500

    Updated dynamic modal

---
+++ scripts.js
@@ -0,0 +1,22 @@
+function updateModal(colNum) {
+  if (colNum == 1) {
+    $('.modal-title').text('What\'s For Lunch?');
+    $('.modal-footer span').text('Beep');
+  }
+  else if (colNum == 2) {
+    $('.modal-title').text('Unredd Posts');
+    $('.modal-footer span').text('Beep');
+  }
+  else if (colNum == 3) {
+    $('.modal-title').text('"Let Me Google That For You" Discord Bot');
+    $('.modal-footer span').text('Beep');
+  }
+  else if (colNum == 4) {
+    $('.modal-title').text('Spooky Ghost');
+    $('.modal-footer span').text('Beep');
+  }
+  else {
+    $('.modal-title').text('Placeholder');
+    $('.modal-footer span').text('Placeholder Two');
+  }
+}
