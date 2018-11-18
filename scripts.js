d15ca69117595b5bd69277d89e246f0130349229
Tyler <twahl36@gmail.com>
2018-11-17 23:26:49 -0500

    Updated carousel images

--- scripts.js
+++ scripts.js
@@ -1,30 +1,38 @@
 function updateModal(colNum) {
   if (colNum == 1) {
-    $('.modal-title').text('What\'s For Lunch?');
-    $('.modal-footer span').text('Beep');
-    $('#car-img1').attr('src', 'images/wfl1.png');
-    $('#car-img2').attr('src', 'images/wfl3.png');
-  }
-  else if (colNum == 2) {
+    $('.carousel').carousel(0)
     $('.modal-title').text('Unredd Posts');
-    $('.modal-footer span').text('Beep');
     $('#car-img1').attr('src', 'images/urp1.png');
     $('#car-img2').attr('src', 'images/urp2.png');
   }
+  else if (colNum == 2) {
+    $('.carousel').carousel(0)
+    $('.modal-title').text('What\'s For Lunch?');
+    $('#car-img1').attr('src', 'images/wfl1.png');
+    $('#car-img2').attr('src', 'images/wfl3.png');
+  }
   else if (colNum == 3) {
+    $('.carousel').carousel(0)
     $('.modal-title').text('"Let Me Google That For You" Discord Bot');
-    $('.modal-footer span').text('Beep');
+    $('#car-img1').attr('src', 'images/bot1.png');
+    $('#car-img2').attr('src', 'images/bot2.png');
   }
   else if (colNum == 4) {
+    $('.carousel').carousel(0)
     $('.modal-title').text('Spooky Ghost');
-    $('.modal-footer span').text('Beep');
+    $('#car-img1').attr('src', 'images/sg2.png');
+    $('#car-img2').attr('src', 'images/sg1.png');
   }
   else if (colNum == 5) {
+    $('.carousel').carousel(0)
     $('.modal-title').text('Indeed Sponsored Jobs Blocker');
-    $('.modal-footer span').text('Beep');
+    $('#car-img1').attr('src', 'images/isjb2.png');
+    $('#car-img2').attr('src', 'images/isjb3.png');
   }
   else {
+    $('.carousel').carousel(0)
     $('.modal-title').text('Placeholder');
-    $('.modal-footer span').text('Placeholder Two');
+    $('#car-img1').attr('src', 'images/placeholder2.png');
+    $('#car-img2').attr('src', 'images/placeholder2.png');
   }
 }
